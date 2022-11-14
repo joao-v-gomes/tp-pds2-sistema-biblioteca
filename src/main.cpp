a5ae5b907167d143191dd1b84567eec18502ae86
Joao Gomes <joao.victor214@gmail.com>
2022-11-14 20:30:33 -0300

    Comecando a testar o menu do Sistema

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,36 @@
+#include "definitions.hpp"
 #include <iostream>
+#include <string>
+
+int main() {
+	std::cout << "---- Bem-vindo ao Sistema de Biblioteca 42!!! -----" << std::endl;
+	std::cout << "\t-- O que voce deseja fazer? --\n\n";
+
+	int opcao;
+
+	std::string textoLogin = "\t0 - Logar como Bilbiotecario \n\t1 - Logar como Cliente \n";
+
+	std::cout << textoLogin << std::endl;
+
+	std::cout << "Digite a opcao escolhida: ";
+
+	std::cin >> opcao;
+
+	switch (opcao) {
+		case LOGIN_BIBLIOTECARIO:
+			std::cout << "Login como bibliotecario" << std::endl;
+			break;
+		case LOGIN_USUARIO:
+			std::cout << "Login como usuario" << std::endl;
+			break;
+		default:
+			std::cout << "Login com problema! Tente novamente!" << std::endl;
+			break;
+	}
+	std::string textoMenuUsuario = "1 - Pesquisar livro pelo nome \n \
+							2 - Pesquisar livro pelo autor \n \
+							3 - Pesquisar livro pelo assunto \n \
+							4 - Pesquisar reservas de livros \n ";
+
+	return 0;
+}
