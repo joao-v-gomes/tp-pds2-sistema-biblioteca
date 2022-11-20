1d36d988ca38f76123ecc1f53d143cde4ed2a85e
Joao Gomes <joao.victor214@gmail.com>
2022-11-20 17:22:26 -0300

    Mudancas no .hpp do menu

--- include/menu.hpp
+++ include/menu.hpp
@@ -1,10 +1,14 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include "definitions.hpp"
 #include <iostream>
+#include <string>
 
 int menuLogin();
-int menuUsuario();
+int menuCliente();
 int menuBibliotecario();
+void menuCadastrarCliente();
+int verificarLogin(std::string usuario, std::string senha);
 
 #endif
